f1dd91a8b4b6d29487845700371271147b933367
2021-upv-isw-3b1-equipo04 <jrpasgar@upv.edu.es>
2022-10-06 12:58:27 +0200

    usuarioClass arreglado

--- ProyectoAngular/clases/UsuarioClass.php
+++ ProyectoAngular/clases/UsuarioClass.php
@@ -19,35 +19,35 @@
             $this -> contrasena = $contrasena;
         }
 
-        public function getId() { return $id; }
+        public function getId() { return $this -> id; }
         public function setId(int $id) { $this -> id = $id; }
 
-        public function getNombre() { return $nombre; }
-        public function setNombre(string $Nombre) { $this -> Nombre = $Nombre; }
+        public function getNombre() { return $this -> nombre; }
+        public function setNombre(string $nombre) { $this -> nombre = $nombre; }
 
-        public function getApellidos() { return $apellidos; }
-        public function setApellidos(string $Apellidos) { $this -> apellidos = $apellidos; }
+        public function getApellidos() { return $this -> apellidos; }
+        public function setApellidos(string $apellidos) { $this -> apellidos = $apellidos; }
 
-        public function getContrasena() { return $contrasena; }
-        public function setContrasena(string $Contrasena) { $this -> contrasena = $contrasena; }
+        public function getContrasena() { return $this -> contrasena; }
+        public function setContrasena(string $contrasena) { $this -> contrasena = $contrasena; }
 
-        public function getCorreo() { return $correo; }
-        public function setCorreo(string $Correo) { $this -> correo = $correo; }
+        public function getCorreo() { return $this -> correo; }
+        public function setCorreo(string $correo) { $this -> correo = $correo; }
 
-        public function getTelefono() { return $telefono; }
-        public function setTelefono($Telefono) { $this -> telefono = $telefono; }
+        public function getTelefono() { return $this -> telefono; }
+        public function setTelefono($telefono) { $this -> telefono = $telefono; }
 
-        public function getFechaNac() { return $fechaNac; }
-        public function setFechaNac(Date $FechaNac) { $this -> fechaNac = $fechaNac; }
+        public function getFechaNac() { return $this -> fechaNac; }
+        public function setFechaNac(DateTime $fechaNac) { $this -> fechaNac = $fechaNac; }
 
-        public function getDni() { return $dni; }
-        public function setDni(string $Dni) { $this -> dni = $dni; }
+        public function getDni() { return $this -> dni; }
+        public function setDni(string $dni) { $this -> dni = $dni; }
 
-        public function getDirPost() { return $dirPost; }
-        public function setDirPost(string $DirPost) { $this -> dirPost = $dirPost; }
+        public function getDirPost() { return $this -> dirPost; }
+        public function setDirPost(string $dirPost) { $this -> dirPost = $dirPost; }
 
-        public function getDirFact() { return $dirFact; }
-        public function setDirFact(string $DirFact) { $this -> dirFact = $dirFact; }
+        public function getDirFact() { return $this -> dirFact; }
+        public function setDirFact(string $dirFact) { $this -> dirFact = $dirFact; }
 
 
       
