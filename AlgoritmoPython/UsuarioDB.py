71f6bfdc227e7ef93e36ec49c9cd4a532f9c480e
cesar <cayucer@inf.upv.es>
2022-10-18 20:35:59 +0200

    Cambios en UsuarioDB y nuevo VueloDB

--- AlgoritmoPython/UsuarioDB.py
+++ AlgoritmoPython/UsuarioDB.py
@@ -5,6 +5,7 @@ from json import JSONEncoder
 from lib2to3.pgen2.token import GREATEREQUAL
 import string
 import psycopg2
+from psycopg2.extras import RealDictCursor
 
 # Conexión a la BDD (importar desde fichero externo?)
 try:
@@ -13,7 +14,7 @@ try:
                                   host="88.17.121.110",
                                   port="5432",
                                   database="bluesky")
-    cursor = connection.cursor()
+    cursor = connection.cursor(cursor_factory = RealDictCursor)
 except Exception as error:
     raise error
 
@@ -68,8 +69,7 @@ def UpdateUsuario(id: int, nombre: string, apellidos: string, correo: string, te
     return usuario
 
 
-def DeleteUsuario(id: int, nombre: string, apellidos: string, correo: string, telefono: string, fechaNac: string,
-                  dni: string, dirPost: string, dirFac: string, contra: string):
+def DeleteUsuario(id: int):
     try:
         sql_delete_query = """Delete from usuario where id = %s"""
         cursor.execute(sql_delete_query, id)
@@ -85,9 +85,29 @@ def DeleteUsuario(id: int, nombre: string, apellidos: string, correo: string, te
 
     return True
         
+def GetUsuarioByCorreo(correo: string):
+    try:
+        postgreSQL_select_Query = "select * from usuario where correo = {s}".format(s = correo)
+        cursor.execute(postgreSQL_select_Query)
 
+        columns = ('id', 'nombre', 'apellidos', 'correo', 'telefono',
+                   'fecha_nacimiento', 'dni', 'direccion_postal', 'direccion_facturacion', 'contrasenya')
+        results = []
+        
+        for usuario in cursor.fetchall():
+            results.append(dict(zip(columns, usuario)))
 
-def GetUsuario(id: string):
+        return json.dumps(results)
+
+    except Exception as error:
+        raise error
+
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+
+def GetUsuarioById(id: string):
     try:
         postgreSQL_select_Query = "select * from usuario where id = %s"
         cursor.execute(postgreSQL_select_Query, id)
@@ -111,3 +131,4 @@ def GetUsuario(id: string):
 
 #except (Exception, psycopg2.Error) as error:
 #        print("Error while fetching data from PostgreSQL", error)
+print(GetUsuarioByCorreo("'prueba'"))
