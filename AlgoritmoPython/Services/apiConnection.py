4849213f06987ddc2bc803e70388be0531661532
Carmen Rozalén <carmenrosalen99@gmail.com>
2022-10-18 11:08:48 +0200

    get Vuelos

--- AlgoritmoPython/Services/apiConnection.py
+++ AlgoritmoPython/Services/apiConnection.py
@@ -1,7 +1,27 @@
-import requests
+from amadeus import Client, ResponseError
 
-BASE_URL = 'test.api.amadeus.com/v3'
+amadeus = Client(
+    client_id='H60c8yIe6mIkrrgNCc48AvkxmRTjpjRR',
+    client_secret='gfqji5m9gPlPtIqi'
+)
 
-res = requests.get(BASE_URL + '/reference-data/locations/hotels/by-city/CUN')
 
-print(res)
+def getHotelByCity(cityCode):
+    try:
+        response = amadeus.get(
+            '/v1/reference-data/locations/hotels/by-city', cityCode=cityCode)
+        return response.data
+    except ResponseError as error:
+        return error
+
+
+def getFlight(originLocationCode, destinationLocationCode, departureDate, adults):
+    try:
+        response = amadeus.get(
+            '/v2/shopping/flight-offers', originLocationCode=originLocationCode, destinationLocationCode=destinationLocationCode, departureDate=departureDate, adults=adults)
+        return response.data
+    except ResponseError as error:
+        return error
+
+
+print(getFlight('SYD', 'BKK', '2022-11-01', 1))
