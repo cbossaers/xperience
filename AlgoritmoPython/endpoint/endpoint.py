9c79f7dddce17b28f35b61c3b16eb745fb3a944b
Cristian <crirodb2@inf.upv.es>
2022-10-22 18:25:28 +0200

    API Rest, funciona a través de internet Cambio de nombres a forma consistente

---
+++ AlgoritmoPython/endpoint/endpoint.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_restful import Api
+import hotel, vuelo
+
+app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(vuelo.Vuelo, '/vuelo')
+api.add_resource(hotel.Hotel, '/hotel')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
---
+++ AlgoritmoPython/endpoint/vuelo.py
@@ -0,0 +1,89 @@
+from flask_restful import Resource, Api, reqparse
+import pandas as pd
+import ast
+
+class Vuelo(Resource):
+    def get(self):
+        data = "Hola Daniel!"
+        return {'data': data}, 200  # return data and 200 OK
+
+    def post(self):
+        parser = reqparse.RequestParser()  # initialize
+        parser.add_argument('userId', required=True)  # add args
+        parser.add_argument('name', required=True)
+        parser.add_argument('city', required=True)
+        args = parser.parse_args()  # parse arguments to dictionary
+
+        # read our CSV
+        data = pd.read_csv('Vuelos.csv')
+
+        if args['userId'] in list(data['userId']):
+            return {
+                'message': f"'{args['userId']}' already exists."
+            }, 409
+        else:
+            # create new dataframe containing new values
+            new_data = pd.DataFrame({
+                'userId': [args['userId']],
+                'name': [args['name']],
+                'city': [args['city']],
+                'Hoteles': [[]]
+            })
+            # add the newly provided values
+            data = data.append(new_data, ignore_index=True)
+            data.to_csv('Vuelos.csv', index=False)  # save back to CSV
+            return {'data': data.to_dict()}, 200  # return data with 200 OK
+
+    def put(self):
+        parser = reqparse.RequestParser()  # initialize
+        parser.add_argument('userId', required=True)  # add args
+        parser.add_argument('location', required=True)
+        args = parser.parse_args()  # parse arguments to dictionary
+
+        # read our CSV
+        data = pd.read_csv('Vuelos.csv')
+        
+        if args['userId'] in list(data['userId']):
+            # evaluate strings of lists to lists !!! never put something like this in prod
+            data['Hoteles'] = data['Hoteles'].apply(
+                lambda x: ast.literal_eval(x)
+            )
+            # select our user
+            user_data = data[data['userId'] == args['userId']]
+
+            # update user's Hoteles
+            user_data['Hoteles'] = user_data['Hoteles'].values[0] \
+                .append(args['location'])
+            
+            # save back to CSV
+            data.to_csv('Vuelos.csv', index=False)
+            # return data and 200 OK
+            return {'data': data.to_dict()}, 200
+
+        else:
+            # otherwise the userId does not exist
+            return {
+                'message': f"'{args['userId']}' user not found."
+            }, 404
+
+    def delete(self):
+        parser = reqparse.RequestParser()  # initialize
+        parser.add_argument('userId', required=True)  # add userId arg
+        args = parser.parse_args()  # parse arguments to dictionary
+        
+        # read our CSV
+        data = pd.read_csv('Vuelos.csv')
+        
+        if args['userId'] in list(data['userId']):
+            # remove data entry matching given userId
+            data = data[data['userId'] != args['userId']]
+            
+            # save back to CSV
+            data.to_csv('Vuelos.csv', index=False)
+            # return data and 200 OK
+            return {'data': data.to_dict()}, 200
+        else:
+            # otherwise we return 404 because userId does not exist
+            return {
+                'message': f"'{args['userId']}' user not found."
+            }, 404
