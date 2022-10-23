4e50362d795fe7461ceaf26ca31f596edb7e1a08
Cristian <crirodb2@inf.upv.es>
2022-10-23 11:01:41 +0200

    algunos cambios de api

--- ProyectoAngular/clases/VueloDB.php
+++ ProyectoAngular/clases/VueloDB.php
@@ -6,21 +6,18 @@
 
     function GetVueloByFechaPrecio(string $precio, string $fechaSalida, string $fechaLlegada) { 
 
-        $data = array(
+        $data = http_build_query(array(
             'precio' => $precio,
             'fechaSalida' => $fechaSalida,
             'fechaLlegada' => $fechaLlegada
-        );
+        ));
 
         $ch = curl_init();
 
         $opts = [
-            CURLOPT_URL => '88.17.26.37:5000/vuelo',
+            CURLOPT_URL => '88.17.26.37:5000/vuelo' . '?' . $data,
             CURLOPT_RETURNTRANSFER => true,
-            //CURLOPT_CUSTOMREQUEST => 'POST',
             CURLOPT_POST => true,
-            //CURLOPT_HTTPHEADER => [ 'Content-Type: application/x-www-form-urlencoded' ],
-            CURLOPT_POSTFIELDS => http_build_query($data)
         ];
 
         curl_setopt_array($ch, $opts);
--- endpoint/vuelo.py
+++ endpoint/vuelo.py
@@ -1,5 +1,5 @@
 from flask import jsonify
-from flask_restful import Resource, reqparse, request
+from flask_restful import Resource, Api, reqparse, request
 import pandas as pd
 import ast
 
@@ -9,9 +9,8 @@ class Vuelo(Resource):
         return {'data': data}, 200  # return data and 200 OK
 
     def post(self):
-        
-        data = request.get_data()
-        return {'data': data}, 200  # return data with 200 OK
+        args = request.args.to_dict()
+        return {"args":args}, 200  # return data with 200 OK
 
     def put(self):
         parser = reqparse.RequestParser()  # initialize
