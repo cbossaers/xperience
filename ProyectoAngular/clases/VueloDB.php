da28a1f1343c45f0a9d6f0ce0e8dbaefa388e5c2
Cristian <crirodb2@inf.upv.es>
2022-10-23 10:07:28 +0200

    Casi listo API

--- ProyectoAngular/clases/VueloDB.php
+++ ProyectoAngular/clases/VueloDB.php
@@ -3,29 +3,35 @@
     function AddVuelo() {  }
     function UpdateVuelo() {  }
     function DeleteVuelo() {  }
+
     function GetVueloByFechaPrecio(string $precio, string $fechaSalida, string $fechaLlegada) { 
+
+        $data = array(
+            'precio' => $precio,
+            'fechaSalida' => $fechaSalida,
+            'fechaLlegada' => $fechaLlegada
+        );
 
         $ch = curl_init();
 
-        $url = '88.17.26.37:5000/vuelo';
+        $opts = [
+            CURLOPT_URL => '88.17.26.37:5000/vuelo',
+            CURLOPT_RETURNTRANSFER => true,
+            //CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POST => true,
+            //CURLOPT_HTTPHEADER => [ 'Content-Type: application/x-www-form-urlencoded' ],
+            CURLOPT_POSTFIELDS => http_build_query($data)
+        ];
 
-        $data = array(
-            "precio" => "125",
-            "fechaSalida" => "13/10",
-            "fechaLlegada"=> "18/10"
-        );
-        
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_HEADER, false);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt_array($ch, $opts);
 
         $resp = curl_exec($ch);
 
         curl_close($ch);
             
         echo $resp;
-}
+    }
 
-getVueloByFechaPrecio("20","10/04","22/10");
+    getVueloByFechaPrecio(20,'10/04','22/10');
+
+?>
