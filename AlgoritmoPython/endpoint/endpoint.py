3b98bfe6368b054a8b5b648151a697d1bc5cc118
2021-upv-isw-3b1-equipo04 <jrpasgar@upv.edu.es>
2022-10-24 02:35:53 +0200

    gets de vuelo, hotel, usuario en proceso

--- AlgoritmoPython/endpoint/endpoint.py
+++ AlgoritmoPython/endpoint/endpoint.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_restful import Api
-import hotel, vuelo
+import hotel, vuelo #, usuario
 
 app = Flask(__name__)
 
@@ -8,6 +8,7 @@ api = Api(app)
 
 api.add_resource(vuelo.Vuelo, '/vuelo')
 api.add_resource(hotel.Hotel, '/hotel')
+#api.add_resource(usuario.Usuario, '/usuario')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
