04fce396fbbe736330ca318a6c8905d5ae093031
Alejandro Palacios Buitrago <apalbui@upv.edu.es>
2022-11-07 18:44:52 +0100

    Header y filtros

--- ProyectoAngular/src/app/app.module.ts
+++ ProyectoAngular/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Componentes/header/header.component';
+import { MainComponent } from './Componentes/main/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
