cf77fd33cb4da6f0a0883094ca0cbba894f5f362
Cristian <crirodb2@inf.upv.es>
2022-11-21 16:18:31 +0100

    Correccion de errores

--- AlgoritmoPython/endpoint/endpoint.py
+++ AlgoritmoPython/endpoint/endpoint.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_restful import Api
-import hotel, vuelo #, usuario
+import hotel, vuelo, usuario
 
 app = Flask(__name__)
 
@@ -8,7 +8,7 @@ api = Api(app)
 
 api.add_resource(vuelo.Vuelo, '/vuelo')
 api.add_resource(hotel.Hotel, '/hotel')
-#api.add_resource(usuario.Usuario, '/usuario')
+api.add_resource(usuario.Usuario, '/usuario')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
--- ProyectoAngular/logic/vuelo.ts
+++ ProyectoAngular/logic/vuelo.ts
@@ -1,6 +1,5 @@
 async function GetVueloByFechaPrecio(precio: number, fechaSalida: Date, fechaLlegada: Date) {
     try {
-      // 👇️ const response: Response
       const response = await fetch('http://88.17.26.37:5000/vuelo', {
         method: 'POST',
         body: JSON.stringify({
@@ -15,22 +14,22 @@ async function GetVueloByFechaPrecio(precio: number, fechaSalida: Date, fechaLle
       });
   
       if (!response.ok) {
-        throw new Error(`Error! status: ${response.status}`);
+        throw new Error('Error! status: ${response.status}');
       }
-  
-      // 👇️ const result: GetVueloByFechaPrecioResponse
+
       const result = await response.json()
   
       console.log('result is: ', JSON.stringify(result, null, 4));
   
       return result;
+
     } catch (error) {
       if (error instanceof Error) {
-        console.log('error message: ', error.message);
+        console.log('Error message: ', error.message);
         return error.message;
       } else {
-        console.log('unexpected error: ', error);
+        console.log('Unexpected error: ', error);
         return 'An unexpected error occurred';
       }
     }
-  }
+}
