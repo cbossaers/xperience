bc8e8ec8dbb55ebe4fe5028018d5a0707008a3bc
Jose Ramon <jrpasgar@upv.edu.es>
2022-12-07 01:04:29 +0100

    avances seguros

--- ProyectoAngular/src/app/app.module.ts
+++ ProyectoAngular/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms"
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Componentes/header/header.component';
 import { MainComponent } from './Componentes/main/main/main.component';
 import { VistaViajeComponent } from './Componentes/vistaViaje/vista-viaje/vista-viaje.component';
+import { LoginComponent } from './Componentes/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MainComponent,
-    VistaViajeComponent
+    VistaViajeComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
