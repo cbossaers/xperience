e9a51c791c412f9d4817549eaa20857598f99ab8
Santiago Cunillera Aparicio <scunapa@upv.edu.es>
2022-12-14 22:45:17 +0100

    visual + carmen

--- ProyectoAngular/src/app/Componentes/main/main/main.component.ts
+++ ProyectoAngular/src/app/Componentes/main/main/main.component.ts
@@ -157,15 +157,21 @@ export class MainComponent implements OnInit {
       elementos[i].value='';          
     }
   }
-
-
-
   realizarPago() {
     this.modalSwitch1 = false;
     this.modalSwitch2 = false;
     this.modalSwitch3 = false;
     this.modalSwitch4 = false;
     this.modalSwitch5 = true;
+    this.modalFiltro = false;
+  }
+
+  volverInicio() {
+    this.modalSwitch1 = true;
+    this.modalSwitch2 = false;
+    this.modalSwitch3 = false;
+    this.modalSwitch4 = false;
+    this.modalSwitch5 = false;
     this.modalFiltro = false;
   }
 }
