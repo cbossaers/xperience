aec534732bb54b5322f00ccf68eb9bc0f62244e3
Cristian <crirodb2@inf.upv.es>
2022-12-15 07:24:49 +0100

    mas cambio

--- AlgoritmoPython/cristian/paq.py
+++ AlgoritmoPython/cristian/paq.py
@@ -1,5 +1,6 @@
 from flask import jsonify
 from flask_restful import Resource, Api, reqparse, request
+import time
 
 import fake as f
 
@@ -8,4 +9,5 @@ class Paq(Resource):
     def post(self):
         args = request.json
         x = f.Fake(args["origen"], args["fechaIda"], args["fechaVuelta"], int(args["presupuesto"]))
+        time.sleep(2)
         return x, 200  # return data with 200 OK
